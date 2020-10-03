001d316665367d191b6ac95ddd906b19b453b3f0
Gaurav Sachdeva <sachdeva.gaurav2911@gmail.com>
2020-10-03 19:05:40 +0530

    deploy

---
+++ precache-manifest.45b86107735d5161fa07e2fc29429650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c39d84e7862e380f6800756caca974f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecb7cedfeadad54d798",
+    "url": "/static/css/main.1adea441.chunk.css"
+  },
+  {
+    "revision": "2471ce0d0cee43149488",
+    "url": "/static/js/2.74fefb50.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.74fefb50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecb7cedfeadad54d798",
+    "url": "/static/js/main.b0b02478.chunk.js"
+  },
+  {
+    "revision": "5c4cd1c30f7b8986333b",
+    "url": "/static/js/runtime-main.fd753eb0.js"
+  }
+]);
